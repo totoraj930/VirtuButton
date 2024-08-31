9f4b23542ed03aa311d7d451d0d092de2267f063
Reona Oshima(totoraj) <totoraj930@yahoo.co.jp>
2024-09-01 00:02:19 +0900

    fieldsを動的に生成するようにした

--- app/src-electron/main/built-in/basic-features/actions/buttonStyle.ts
+++ app/src-electron/main/built-in/basic-features/actions/buttonStyle.ts
@@ -3,8 +3,32 @@ import {
   BooleanField,
   NumberField,
   PluginAction,
+  PluginFields,
 } from '@virtu-button/common/Plugin';
 import { pluginInitProps } from '..';
+
+type FieldMap = {
+  forceNext: BooleanField;
+  index: NumberField;
+};
+
+const fields: PluginFields<FieldMap> = (values) => {
+  return {
+    index: {
+      type: 'number',
+      name: 'スタイル番号',
+      default: values?.index ?? 1,
+      min: 1,
+      max: 5,
+      disabled: values?.forceNext,
+    },
+    forceNext: {
+      type: 'boolean',
+      name: '番号指定を無視して次のスタイルへ進める',
+      default: values?.forceNext ?? false,
+    },
+  };
+};
 
 export const buttonStyleAction: PluginAction<{
   index: NumberField;
@@ -13,20 +37,7 @@ export const buttonStyleAction: PluginAction<{
   id: 'button-style',
   name: 'ボタンスタイル切り替え',
   description: 'ボタンのスタイルを切り替えます',
-  fields: {
-    index: {
-      type: 'number',
-      name: 'スタイル番号',
-      default: 1,
-      min: 1,
-      max: 5,
-    },
-    forceNext: {
-      type: 'boolean',
-      name: '番号指定を無視して次のスタイルへ進める',
-      default: false,
-    },
-  },
+  fields,
   run: async (values, payload) => {
     if (!payload.from.buttonId) return false;
     const button = getPageItem(payload.from.buttonId);
